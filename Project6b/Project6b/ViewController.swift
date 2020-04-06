aac4be0c0a4f301dab6c8117ee2281fa8610a517
Daniel Gx <danielgx@MacBook-Pro-de-ioasys.local>
2020-04-06 11:08:00 -0300

    pushing project6b

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -85,19 +85,43 @@ extension ViewController {
         view.addSubview(label3)
         view.addSubview(label4)
         view.addSubview(label5)
-                
+    
+        usingAnchors()
+        
+    }
+    
+    func usingVFLConstraints() {
         let viewDictionary = [ "label1" : label1,
                                "label2" : label2,
                                "label3" : label3,
                                "label4" : label4,
                                "label5" : label5
         ]
-        
+        let metrics = ["labelHeight" : 88]
+
         for label in viewDictionary.keys {
             view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewDictionary))
         }
+
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewDictionary))
+    }
+    
+    func usingAnchors() {
+        var previous: UILabel?
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewDictionary))
+        for label in [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+//            label.heightAnchor.constraint(equalToConstant: (view.bounds.height/5) - 10).isActive = true
+            label.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            previous = label
+        }
     }
 }
 
