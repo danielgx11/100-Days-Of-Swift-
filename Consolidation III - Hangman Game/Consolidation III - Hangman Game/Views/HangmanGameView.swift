fbf95a80cf7a850cdfa2563fb745fa9c069f49fe
Daniel Gx <danielgx@Daniels-CPU.local>
2020-04-22 18:23:43 -0300

    pushing activate buttons and answers

--- Consolidation III - Hangman Game/Consolidation III - Hangman Game/Views/HangmanGameView.swift
+++ Consolidation III - Hangman Game/Consolidation III - Hangman Game/Views/HangmanGameView.swift
@@ -17,6 +17,27 @@ class HangmanGameView: UIViewController {
     var allWordsPortuguese = [String]()
     var usedEnglishWords = [String]()
     var usedPortugueseWords = [String]()
+    var characterButtons = [UIButton]()
+    let allLetters = (65...90).map {String(UnicodeScalar($0))}
+        func randomLetter() -> String {
+            return allLetters.randomElement()!
+    }
+    var activatedButtons = [UIButton]()
+    var solution = String()
+    var usedLetters = String()
+    var errorsCount = 0 {
+        didSet {
+            self.errorsNavigationItem.title = "Errors: \(self.errorsCount)"
+        }
+    }
+    var chancesRemaining = 6 {
+        didSet {
+            self.navigationItem.title = "Chances Remaining: \(self.chancesRemaining)"
+            if self.chancesRemaining == 0 {
+                 alertController("Ops", message: "Looks like your chances are gone, good luck and try again.")
+            }
+        }
+    }
     
     let buttonsView: UIView = {
         let buttonsView = UIView()
@@ -31,6 +52,12 @@ class HangmanGameView: UIViewController {
         scoreNavigationItem.tintColor = .black
         return scoreNavigationItem
     }()
+    var errorsNavigationItem: UIBarButtonItem = {
+        let errorsNavigationItem = UIBarButtonItem()
+        errorsNavigationItem.title = "Errors: 0"
+        errorsNavigationItem.tintColor = .black
+        return errorsNavigationItem
+    }()
     var score = 0 {
         didSet {
             self.scoreNavigationItem.title = "Score: \(self.score)"
@@ -39,7 +66,6 @@ class HangmanGameView: UIViewController {
     
     var correctAnswer: UITextField = {
         let correctAnswer = UITextField()
-        correctAnswer.insertText("Resposta")
         correctAnswer.isUserInteractionEnabled = false
         correctAnswer.backgroundColor = .yellow
         correctAnswer.translatesAutoresizingMaskIntoConstraints = false
@@ -49,12 +75,11 @@ class HangmanGameView: UIViewController {
     
     var currentAnswer: UITextField = {
         let currentAnswer = UITextField()
-        currentAnswer.text = "Tentativa"
         currentAnswer.isUserInteractionEnabled = false
         currentAnswer.backgroundColor = .green
-        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
         currentAnswer.placeholder = "Tap letter to guess"
         currentAnswer.textAlignment = .center
+        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
         return currentAnswer
     }()
     
@@ -62,6 +87,7 @@ class HangmanGameView: UIViewController {
         var submit = UIButton(type: .system)
         submit.translatesAutoresizingMaskIntoConstraints = false
         submit.setTitle("SUBMIT", for: .normal)
+        submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
         return submit
     }()
     
@@ -69,8 +95,60 @@ class HangmanGameView: UIViewController {
         var clear = UIButton(type: .system)
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("CLEAR", for: .normal)
+        clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
         return clear
     }()
+    
+    // MARK: - Actions
+    
+    @objc func userLose(_ sender: UIButton) {
+        
+    }
+    
+    @objc func characterTapped(_ sender: UIButton) {
+        guard let characterTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(characterTitle)
+        activatedButtons.append(sender)
+    }
+    
+    @objc func submitTapped(_ sender: UIButton) {
+        
+        let word = solution.lowercased()
+        let usedWord = String((currentAnswer.text?.first?.lowercased())!)
+        var promptWord = correctAnswer.text!
+        var nextReplies = ""
+        
+        if word.contains(usedWord) {
+            score += 1
+        } else {
+            chancesRemaining -= 1
+            errorsCount += 1
+            score -= 1
+        }
+        
+        for (index, letter) in word.enumerated() {
+            let strLetter = String(letter)
+            if strLetter.contains(usedWord) {
+                nextReplies += strLetter
+                promptWord.remove(at: promptWord.index(promptWord.startIndex, offsetBy: index))
+                promptWord.insert(letter, at: promptWord.index(promptWord.startIndex, offsetBy: index))
+                correctAnswer.text = promptWord
+            } else {
+                continue
+            }
+        }
+        currentAnswer.text?.removeAll()
+    }
+    
+    @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+        
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+        
+        activatedButtons.removeAll()
+    }
 
     // MARK: - Life Cycle
 
@@ -79,9 +157,40 @@ class HangmanGameView: UIViewController {
         setupUI()
         recoverWords(language: "Profissões")
         customizeNavigationController()
+        settingPortugueseWord()
     }
     
     // MARK: - Methods
+    
+    func alertController(_ title: String, message: String, lose: Bool = false) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if lose {
+            
+        } else {
+            ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        }
+        present(ac, animated: true)
+    }
+    
+    func settingEnglishWord() {
+        let randomIndex = arc4random()
+        let word = allWordsEnglish[randomIndex.hashValue]
+        correctAnswer.text = word
+    }
+    
+    func settingPortugueseWord() {
+        var promptWord = ""
+        let randomIndex = Int.random(in: 1...99)
+        let word = allWordsPortuguese[randomIndex]
+        usedLetters = correctAnswer.text!
+        correctAnswer.text = word
+        solution = word
+
+        for _ in word {
+            promptWord += "?"
+            correctAnswer.text = promptWord
+        }
+    }
     
     func recoverWords(language path: String) {
         DispatchQueue.global().async { [weak self] in
@@ -89,8 +198,10 @@ class HangmanGameView: UIViewController {
                 if let hangmanWords = try? String(contentsOf: hangmanWordsURL) {
                     if path == "Jobs" {
                         self?.allWordsEnglish = hangmanWords.components(separatedBy: "\n")
+                        self?.allWordsEnglish.shuffle()
                     } else if path == "Profissões" {
                         self?.allWordsPortuguese = hangmanWords.components(separatedBy: "\n")
+                        self?.allWordsPortuguese.shuffle()
                     }
                 }
             } else {
@@ -101,6 +212,8 @@ class HangmanGameView: UIViewController {
     
     func customizeNavigationController() {
         navigationItem.leftBarButtonItem = scoreNavigationItem
+        navigationItem.title = "Chances Remaining: \(chancesRemaining)"
+        navigationItem.rightBarButtonItem = errorsNavigationItem
     }
     
 }
@@ -144,5 +257,41 @@ extension HangmanGameView {
         buttonsView.topAnchor.constraint(equalTo: submit.bottomAnchor, constant: 20).isActive = true
         buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20).isActive = true
         
+        /// Buttons activate
+        
+        let widht = 80
+        let height = 40
+        
+        debugPrint(widht)
+        debugPrint(height)
+        
+        var index = 0
+        
+        for row in 0..<5 {
+            for col in 0..<5 {
+                let characterButton = UIButton(type: .system)
+                characterButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+                characterButton.tintColor = .black
+                characterButton.setTitle(allLetters[index], for: .normal)
+                characterButton.addTarget(self, action: #selector(characterTapped), for: .touchUpInside)
+                
+                let frame = CGRect(x: col * widht, y: row * height, width: widht, height: height)
+                debugPrint(frame)
+                characterButton.frame = frame
+                
+                buttonsView.addSubview(characterButton)
+                characterButtons.append(characterButton)
+                index += 1
+            }
+        }
+        let zButton = UIButton(type: .system)
+        zButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        zButton.tintColor = .black
+        zButton.setTitle("Z", for: .normal)
+        zButton.addTarget(self, action: #selector(characterTapped), for: .touchUpInside)
+        let frame = CGRect(x: 2 * widht, y: 5 * height, width: widht, height: height)
+        debugPrint(frame)
+        zButton.frame = frame
+        buttonsView.addSubview(zButton)
     }
 }
