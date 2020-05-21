6d52b33eec1dd6e05a2599e458e578486b42ce6b
Daniel Gx <danielgx@Daniels-CPU.local>
2020-05-21 12:26:02 -0300

    Challenge Finished

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,126 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Daniel Gx on 21/05/20.
+//  Copyright © 2020 Daniel Gx. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    // MARK: - Properties
+    
+    var starfield: SKEmitterNode = {
+        var starfield = SKEmitterNode(fileNamed: "starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        starfield.zPosition = -1
+        return starfield
+    }()
+    var player: SKSpriteNode = {
+        var player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        return player
+    }()
+    
+    var scoreLabel: SKLabelNode = {
+        var scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        return scoreLabel
+    }()
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    let possibleEnemies = ["ball", "hammer", "tv"]
+    var isGameOver = false
+    var gameTimer: Timer?
+    
+    // MARK: - Actions
+    
+    @objc func createEnemy() {
+        guard let enemy = possibleEnemies.randomElement() else { return }
+
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
+    }
+    
+    // MARK: - Game Cycle
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+        addChild(starfield)
+        addChild(player)
+        addChild(scoreLabel)
+        
+        score = 0
+        setupContact()
+        setupEnemies()
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        assert(children.isEmpty == false, "Children array is empty")
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+        if !isGameOver {
+            score += 1
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        var location = touch.location(in: self)
+        
+        if location.y < 100 {
+            location.y = 100
+        } else if location.y > 668 {
+            location.y = 668
+        }
+        
+        player.position = location
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let explosion = SKEmitterNode(fileNamed: "explosion")
+        explosion?.position = player.position
+        addChild(explosion!)
+        
+        player.removeFromParent()
+        isGameOver = true
+    }
+    
+    // MARK: - Methods
+    
+    func setupEnemies() {
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    }
+}
+
+// MARK: - SKPhysicsContactDelegate
+
+extension GameScene: SKPhysicsContactDelegate {
+    private func setupContact() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+    }
+}
