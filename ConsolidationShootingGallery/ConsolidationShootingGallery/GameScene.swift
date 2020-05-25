2b6f0a2bc73e0bfc4803686f8881792466278857
Daniel Gx <danielgx@Daniels-CPU.local>
2020-05-25 10:59:04 -0300

    Challenge Finished

---
+++ ConsolidationShootingGallery/ConsolidationShootingGallery/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  ConsolidationShootingGallery
+//
+//  Created by Daniel Gx on 22/05/20.
+//  Copyright © 2020 Daniel Gx. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
