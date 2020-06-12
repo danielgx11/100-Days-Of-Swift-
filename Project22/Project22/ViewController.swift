7ae0907bbc19382180cd54a0f004df941af5407c
Daniel Gx <danielgx@Daniels-CPU.local>
2020-06-12 11:29:39 -0300

    Challenge Finished

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var distanceReading: UILabel!
     var locationManager: CLLocationManager?
+    var beaconLocalized: Bool = false
+    
+    var circleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.widthAnchor.constraint(equalToConstant: 256).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 256).isActive = true
+        view.layer.cornerRadius = 128
+        view.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     
     // MARK: - Life Cycle
@@ -23,18 +35,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupUIFeatures()
         setupLocation()
+        view.addSubview(circleView)
     }
     
     // MARK: - Methods
     
     func startScanning() {
         /// WARNING: Put your beacon data below!!!
-        let uuid = UUID(uuidString: "EE895FB2-86BE-4CEE-80ED-F0CBC7FAD1AE")!
-        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 16808, minor: 19400, identifier: "MyBeacon")
+        let firstuuid = UUID(uuidString: "972E5347-88F2-48FF-8841-13C535A28C76")!
+        let firstBeaconRegion = CLBeaconRegion(uuid: firstuuid, major: 16808, minor: 19400, identifier: "MyBeacon")
         ///
+        let seconduuid = UUID(uuidString: "E1D7F24E-3A06-4D19-B012-29F9ABD90A53")!
+        let secondBeaconRegion = CLBeaconRegion(uuid: seconduuid, major: 16808, minor: 19400, identifier: "SecondBeacon")
         
-        locationManager?.startMonitoring(for: beaconRegion)
-        locationManager?.startRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
+        locationManager?.startMonitoring(for: secondBeaconRegion)
+        locationManager?.startRangingBeacons(satisfying: firstBeaconRegion.beaconIdentityConstraint)
+    }
+    
+    func showFirstDetection(identifier: String) {
+        if beaconLocalized {
+            beaconLocalized = false
+            let ac = UIAlertController(title: "Beacon detected", message: identifier, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
 }
 
@@ -62,31 +86,39 @@ extension ViewController: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        
         if let beacon = beacons.first {
             update(distance: beacon.proximity)
+            showFirstDetection(identifier: region.identifier)
         } else {
             update(distance: .unknown)
         }
     }
     
     func update(distance: CLProximity) {
+        
         UIView.animate(withDuration: 1) {
             switch distance {
             case .far:
                 self.view.backgroundColor = UIColor.blue
                 self.distanceReading.text = "FAR"
+                self.circleView.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
                 
             case .near:
                 self.view.backgroundColor = UIColor.orange
                 self.distanceReading.text = "NEAR"
+                self.circleView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
                 
             case .immediate:
                 self.view.backgroundColor = UIColor.red
                 self.distanceReading.text = "RIGHT HERE"
+                self.circleView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
                 
             default:
                 self.view.backgroundColor = UIColor.gray
                 self.distanceReading.text = "UNKNOWN"
+                self.circleView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+
             }
         }
     }
